a5424050e0fc71db83de9c4d7574f106901f41cb
yukatan1701 <yukki.lapenko@gmail.com>
2019-04-10 17:58:23 +0600

    Changes.

--- H1/old_versions/gauss-sla.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import scipy.linalg as sla
-
-n = int(input())
-a = np.random.rand(n, n)
-f = [val for val in range(n)]
-for i in range(n):
-	a[i][i] = sum([abs(val) for val in a[i]]) + abs(a[i][i])
-print(sla.solve(a, f))
--- H1/old_versions/tmp.py
+++
@@ -1,8 +0,0 @@
-import scipy.linalg as sl
-
-A = [ [0 , 3 , 1] ,
-      [4 , 3 , 2] ,
-      [1 , 1 , 0] ]
-f = [10 , 10 , 8]
-x = sl.solve_banded ((1 , 1) , A , f )
-print ( x )
